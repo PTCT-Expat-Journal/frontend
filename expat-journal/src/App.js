c71a12e889e4ef03c31c8db334f41fa6fc15bcb8
Jorge Jimenez <calenocam24@gmail.com>
2020-07-23 19:01:53 -0400

    did some styles, and nav, footer

--- expat-journal/src/App.js
+++ expat-journal/src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom'
+import Login from "./components/Login";
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+      <Header />
+      <Route exact path="/login">
+        <Login />
+      </ Route> 
+      <Footer />
+      </>
     </div>
+    
   );
 }
 
